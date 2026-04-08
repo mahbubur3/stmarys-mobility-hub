f37776c0f7db1ea32e9ba51f7f5ca374460a99fc
mahbubur3 <mahbubur9976@gmail.com>
2026-04-08 10:32:45 +0100

    added navbar and edited homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>St Mary's Mobility Hub</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/options">Transit Options</Link></li>
+        <li><Link to="/fare">Fare Calculator</Link></li>
+        <li><Link to="/finder">Route Finder</Link></li>
+        <li><Link to="/updates">Live Updates</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Home Page</h1>;
+    return (
+        <div>
+            <h1>Welcome! St Mary's Urban Mobility Hub</h1>
+            <p>Plan your journey using different transport options such as bus, rail, and walking.</p>
+        </div>
+    )
 }
 
 export default Home;
